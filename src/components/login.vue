78838d7d66f46842595ea4a8bc9dc9b38fd0bfb3
Gabriel Zeisz <zei17753@spengergasse.at>
2019-12-12 08:08:27 +0100

    few steps into login and calendar

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,51 +1,33 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
+              <v-toolbar color="primary" dark flat>
                 <v-toolbar-title>Login</v-toolbar-title>
                 <v-spacer></v-spacer>
 
 
               </v-toolbar>
               <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Login"
-                    name="login"
-                    prepend-icon="person"
-                    type="text"
-                  ></v-text-field>
 
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    prepend-icon="lock"
-                    type="password"
-                  ></v-text-field>
+
+
+                <h1>{{daten}}</h1>
+
+
+
+                <v-form>
+                  <v-text-field label="Login" name="login" prepend-icon="person" type="text"></v-text-field>
+
+                  <v-text-field id="password" label="Password" name="password" prepend-icon="lock" type="password"></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
+                <v-btn color="primary" @click="loadthedata()">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -62,6 +44,18 @@
     },
     data: () => ({
       drawer: null,
+      daten: {}
     }),
+    methods: {
+      loadthedata() {
+        var self = this
+        this.$http.get('http://localhost:8081/').then((response) => {
+          if (response['status'] == 200) {
+            self.daten = response.data
+          }
+        })
+
+      }
+    }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import routes from './routes'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.xhr = { withCredentials : true };
 
 const router = new VueRouter({routes});
 
